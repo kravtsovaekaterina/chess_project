3f00c04a167f276c8f84367b28053ed4e148b82a
Dima <dkravtsov@qtb.io>
2021-04-10 03:12:54 +0300

    Pandas added.

--- demo_app.py
+++ demo_app.py
@@ -1,14 +1,18 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 with st.echo(code_location='below'):
     st.title("Hello, World!")
     """
     This is a test.
     """
+    data = pd.read_csv('games.csv')
     x = np.linspace(0, 10, 500)
     fig = plt.figure()
     plt.plot(x, np.sin(x))
     plt.ylim(-2, 2)
     st.pyplot(fig)
+
+    data[:10]
